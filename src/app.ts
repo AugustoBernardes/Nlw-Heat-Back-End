0faeb8f320a3d294dfa230cdc6fb0425254abc89
Augusto <augustobdev@outlook.com>
2022-03-14 16:19:52 -0300

    feature - Login with GitHub

--- src/app.ts
+++ src/app.ts
@@ -8,4 +8,16 @@ const PORT = process.env.PORT
 
 const app = express();
 
+
+// Login with GitHub
+app.get("/github", (request,response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback", (request,response) =>{
+    const { code } = request.query
+
+    return response.json(code)
+})
+
 app.listen(PORT, () => { console.log(`Server is running on PORT:${PORT}`)})
