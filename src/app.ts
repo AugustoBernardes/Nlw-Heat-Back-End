8be1c442bb9b3eb2a3284a66e1acce7fab41c008
Augusto <augustobdev@outlook.com>
2022-03-15 13:52:13 -0300

    fix - Console.log message changed

--- src/app.ts
+++ src/app.ts
@@ -12,4 +12,4 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(PORT, () => { console.log(`Server is running on PORT:${PORT}`)})
+app.listen(PORT, () => { console.log(`🚀 Server is running on PORT:${PORT}`)})
