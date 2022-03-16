826f00bc88d4a81e5c5e2b4982a0563e3ad8957a
Augusto <augustobdev@outlook.com>
2022-03-16 13:57:28 -0300

    feature - Route display last messages ready

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageControllet";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController"
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated"
 
 const router = Router();
@@ -8,6 +9,7 @@ const router = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 const createMessageController = new CreateMessageController();
+const getLast3MessagesController = new GetLast3MessagesController();
 
 // Login with GitHub
 // =====================
@@ -27,6 +29,7 @@ router.post("/authenticate",  authenticateUserController.handle)
 
 // Messages
 // ======================
+router.get("/messages/last3", getLast3MessagesController.handle)
 router.post("/messages",ensureAuthenticated,createMessageController.handle)
 
 export { router }
